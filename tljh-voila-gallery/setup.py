9cbcf28dc62980ddfd91a61bfa5fc00c9d13f0f2
Hande Gözükan <hande.gozukan@inria.fr>
2020-11-12 17:40:07 +0100

    change project name

--- tljh-voila-gallery/setup.py
+++ tljh-voila-gallery/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tljh-voila-gallery",
+    name="neurolang-gallery",
     version="0.0.1",
     entry_points={
         "tljh": ["tljh_voila_gallery = tljh_voila_gallery"],
